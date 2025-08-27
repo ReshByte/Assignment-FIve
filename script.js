dc9105b155b62893d81bc28c5dc91ba067491c0d
ReshByte <redwanreshat@gmail.com>
2025-08-27 23:16:07 +0600

    Javascript Call

--- script.js
+++ script.js
@@ -35,3 +35,358 @@ for (const btn of greyButton) {
   })
 }
 
+
+
+//Calling...
+
+const savedData = [];
+
+
+document.getElementById('national-emergency')
+.addEventListener('click',function(e){
+  e.preventDefault();
+  
+  
+
+const data = {
+  name: 'National Emergency Number',
+  phone: 999,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+
+document.getElementById('police-helpline')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  
+
+const data = {
+  name: 'Police Helpline Number',
+  phone: 999,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+
+document.getElementById('fire-down')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+
+
+const data = {
+  name: 'Fire Service Number',
+  phone: 999,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+
+document.getElementById('ambulance')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+
+
+const data = {
+  name: 'Ambulance Service',
+  phone: 1994-999999,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+document.getElementById('women-child')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  
+
+const data = {
+  name: 'Women & Child Helpline',
+  phone: 109,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+document.getElementById('anti-corruption')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  console.log("OKay Bhai")
+
+const data = {
+  name: 'Anti-Corruption Helpline',
+  phone: 106,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+document.getElementById('electricity')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  
+
+const data = {
+  name: 'Electricity Helpline',
+  phone: 16216,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+document.getElementById('brac-helpline')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  
+
+const data = {
+  name: 'Brac Helpline',
+  phone: 16445,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+document.getElementById('bd-railway')
+.addEventListener('click',function(e){
+  e.preventDefault();
+
+  
+
+const data = {
+  name: 'Bangladesh Railway Helpline',
+  phone: 163,
+  date: new Date().toLocaleTimeString()
+}
+
+savedData.push(data)
+
+const callHistory =  document.getElementById('call-history');
+callHistory.innerText = ""
+
+for(const data of savedData){
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div class="flex justify-between items-center h-[80px] p-[10px] bg-[#FAFAFA] rounded-[8px] mx-[18px] mb-[18px]">
+            <div>
+                <h1 class="inter-font font-semibold text-black w-[170px]">${data.name}</h1>
+                <p class="roboto-font text-[16px] text-[#5C5C5C]">${data.phone}</p>
+            </div>
+            <p class="roboto-font text-[#5C5C5C] text-[16px] ">${data.date}</p>
+           </div>
+  `
+
+  callHistory.appendChild(div)
+}
+
+})
+
+
+
+
+
+
+
+
